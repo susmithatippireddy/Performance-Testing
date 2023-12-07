ac09a7674d92baec34f298e015090dd38e9f3c10
susmithatippireddy <145751489+susmithatippireddy@users.noreply.github.com>
2023-12-07 15:49:50 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+#!/bin/bash -e
+#define variables to store info
+version=5.3
+scriptname="CSVSample"
+# override the HEAP settings and run the jmeter script.
+JVM_ARGS="-Xms512m -Xmx2048m" jmeter -n -t JmeterDockerSc1.jmx -l /jmeter.jtl 2>&1
+java -jar /opt/apache-jmeter-${version}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type AggregateReport --input-jtl /jmeter.jtl --generate-csv /results/results.csv 2>&1
+cat /results/results.csv
