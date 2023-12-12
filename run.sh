78cef4d127425cbfa45ae07836c76e25395af04c
susmithatippireddy <145751489+susmithatippireddy@users.noreply.github.com>
2023-12-12 14:53:57 +0530

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,9 +1,23 @@
 #!/bin/bash -e
-#!/bin/bash -e
-#define variables to store info
+# Define variables
 version=5.3
 scriptname="CSVSample"
-# override the HEAP settings and run the jmeter script.
-JVM_ARGS="-Xms512m -Xmx2048m" jmeter -n -t JmeterDockerSc1.jmx -l /jmeter.jtl 2>&1
-java -jar /opt/apache-jmeter-${version}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type AggregateReport --input-jtl /jmeter.jtl --generate-csv /results/results.csv 2>&1
-cat /results/results.csv
+
+# Set JMeter home directory
+JMETER_HOME="/opt/apache-jmeter-${version}"
+
+# Create necessary folders and clean up the HTML report directory
+mkdir -p /jmeter/html-report
+rm -rf /jmeter/html-report/*
+
+# Override the HEAP settings and run the JMeter script
+JVM_ARGS="-Xms512m -Xmx2048m" ${JMETER_HOME}/bin/jmeter -n -t ${JMETER_HOME}/bin/examples/${scriptname}.jmx -l /jmeter.jtl 2>&1
+
+# Generate HTML report using JMeter Plugins Manager
+${JMETER_HOME}/bin/jmeter -g /jmeter.jtl -o /jmeter/html-report
+
+# List contents of the HTML report directory
+ls -R /jmeter/html-report
+
+# Additional command to print the contents of the CSV report
+#cat /jmeter/html-report/results.csv
